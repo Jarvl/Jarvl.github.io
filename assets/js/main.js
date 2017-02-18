cb16b3dbacf9952367470133604f14889e7797b8
Andrew Jarvis <ajarvis7771@gmail.com>
2017-02-18 18:28:14 -0500

    Added social icons, toggling for color schemes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,81 @@
 var name = "ANDREW JARVIS";
 var nameArray = name.split("");
-var startIndex = 0;
+var startNameIndex = 0;
+var bgColorIndex = 0;
+var bgIncrementer = Math.random() < 0.5 ? -1 : 1;
+var colorSchemeIndex = 0;
+var colorSchemes = {
+    default: [
+        '#26A69A',
+        '#FBC02D',
+        '#42A5F5',
+        '#7CB342',
+        '#E57373',
+        '#03A9F4',
+        '#C0CA33',
+        '#00ACC1',
+        '#FF6F00',
+        '#4CAF50'
+    ],
+    vaporwave: [
+        '#FF6AD5',
+        '#AD8CFF',
+        '#94D0FF',
+        '#C774E8',
+        '#8795E8'
+    ]
+};
+var colorSchemeNames = {
+    default: 'vanilla',
+    vaporwave: 'a e s t h e t i c'
+};
 
-var bgClasses = [
-    'red-bg',
-    'indigo-bg',
-    'blue-bg',
-    'green-bg',
-    'yellow-bg',
-    'orange-bg',
-    'brown-bg',
-];
-var currentBgClass = $("body").attr("class");
+var colorSchemesArray = Object.values(colorSchemes);
+var colorSchemeNamesKeys = Object.keys(colorSchemeNames);
+var colorSchemeNamesValues = Object.values(colorSchemeNames);
+
+var currentColorScheme;
+
+function getNextColorSchemeIndex() {
+    return stepArrayIndex(colorSchemeNamesKeys, colorSchemeIndex+1);
+}
+
+$(function() {
+    var initialColorSchemeName = $("#bgColorToggle").data('color-scheme');
+    currentColorScheme = colorSchemes[initialColorSchemeName];
+
+    for (var i = 0; i < colorSchemesArray.length; i++) {
+        if (colorSchemesArray[i] === currentColorScheme) {
+            colorSchemeIndex = i;
+            break;
+        }
+    }
+
+    // Set color scheme text
+    $("#bgColorToggle").text(colorSchemeNamesValues[getNextColorSchemeIndex()]);
+
+    // Set random background initially
+    setRandomBackground();
+    startNameInterval(15, 7, function() {
+        startBgInterval(2000);
+    });
+
+    $("#bgColorToggle").click(function() {
+        // Get the current color scheme and change it
+        colorSchemeIndex = stepArrayIndex(colorSchemesArray, colorSchemeIndex+1);
+
+        // Set current color scheme values
+        currentColorScheme = colorSchemesArray[colorSchemeIndex];
+        // Reset color index
+        bgColorIndex = getRandomIndex(currentColorScheme);
+
+        // Set color scheme name and text
+        $(this).data('color-scheme', colorSchemeNamesKeys[getNextColorSchemeIndex()]);
+        $(this).text(colorSchemeNamesValues[getNextColorSchemeIndex()]);
+    });
+});
+
+
 
 function generateName(tick, delay, separation) {
     // Start filling letters after 10 ticks
@@ -28,11 +92,11 @@ function generateName(tick, delay, separation) {
             generatedName += nameArray[i];
         }
 
-        startIndex = generateStep+1;
+        startNameIndex = generateStep+1;
     }
 
     // Generate the remaining letters
-    for (var j = startIndex; j < name.length; j++) {
+    for (var j = startNameIndex; j < name.length; j++) {
         // Only generate new characters for characters, not spaces
         generatedName += (name.charAt(j) == " ") ? " " : possible.charAt(getRandomIndex(possible));
     }
@@ -40,40 +104,50 @@ function generateName(tick, delay, separation) {
     return generatedName;
 }
 
-function setBackground() {
-    $("body").removeClass(currentBgClass);
-    var idx = getRandomIndex(bgClasses);
+function setBackground(index) {
+    $("body").css('background-color', currentColorScheme[index]);
+}
 
-    while (currentBgClass == bgClasses[idx]) {
-        idx = getRandomIndex(bgClasses);
-    }
-
-    currentBgClass = bgClasses[idx];
-    $("body").addClass(currentBgClass);
+function setRandomBackground() {
+    bgColorIndex = getRandomIndex(currentColorScheme);
+    // Set background initially
+    setBackground(bgColorIndex);
 }
 
 function getRandomIndex(theArray) {
     return Math.floor(Math.random() * theArray.length);
 }
 
-$(function() {
+function startBgInterval(interval) {
+    // Animate after name is filled in
+    $("body").addClass("animate-bg");
+    setRandomBackground();
+
+    window.bgInterval = setInterval(function() {
+        // Change bg color index, keep within range of array indicies
+        bgColorIndex = stepArrayIndex(currentColorScheme, bgColorIndex+bgIncrementer);
+        setBackground(bgColorIndex);
+    }, interval);
+}
+
+function stepArrayIndex(theArray, theIndex) {
+    if (theIndex < 0) theIndex = theArray.length-1;
+    else if (theIndex >= theArray.length) theIndex = 0;
+    return theIndex;
+}
+
+function startNameInterval(delay, separation, cb) {
     var tick = 0;
-    var generateInterval = setInterval(function() {
-        var date = new Date();
+    window.nameInterval = setInterval(function() {
         // Set name
-        var generatedName = generateName(tick, 15, 8);
+        var generatedName = generateName(tick, delay, separation);
         $("#name").text(generatedName);
+
         // Stop interval when name is filled in
-        if (generatedName == name) clearInterval(generateInterval);
+        if (generatedName == name) {
+            clearInterval(window.nameInterval);
+            cb();
+        }
         tick++;
     }, 40);
-
-    setTimeout(function() {
-        setBackground();
-        var bgInterval = setInterval(function() {
-            setBackground();
-        }, 8000);
-    }, 4575);
-
-
-});
+}
