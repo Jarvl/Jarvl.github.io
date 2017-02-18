636cdc2d69b240b5893987261fd8ebc0b68d6ba9
Andrew Jarvis <ajarvis7771@gmail.com>
2017-02-18 08:05:29 -0500

    Added background animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,17 @@ var name = "ANDREW JARVIS";
 var nameArray = name.split("");
 var startIndex = 0;
 
+var bgClasses = [
+    'red-bg',
+    'indigo-bg',
+    'blue-bg',
+    'green-bg',
+    'yellow-bg',
+    'orange-bg',
+    'brown-bg',
+];
+var currentBgClass = $("body").attr("class");
+
 function generateName(tick, delay, separation) {
     // Start filling letters after 10 ticks
     var generateStep = Math.floor((tick - delay) / separation);
@@ -23,21 +34,46 @@ function generateName(tick, delay, separation) {
     // Generate the remaining letters
     for (var j = startIndex; j < name.length; j++) {
         // Only generate new characters for characters, not spaces
-        generatedName += (name.charAt(j) == " ") ? " " : possible.charAt(Math.floor(Math.random() * possible.length));
+        generatedName += (name.charAt(j) == " ") ? " " : possible.charAt(getRandomIndex(possible));
     }
 
     return generatedName;
 }
 
+function setBackground() {
+    $("body").removeClass(currentBgClass);
+    var idx = getRandomIndex(bgClasses);
+
+    while (currentBgClass == bgClasses[idx]) {
+        idx = getRandomIndex(bgClasses);
+    }
+
+    currentBgClass = bgClasses[idx];
+    $("body").addClass(currentBgClass);
+}
+
+function getRandomIndex(theArray) {
+    return Math.floor(Math.random() * theArray.length);
+}
+
 $(function() {
     var tick = 0;
-    var interval = setInterval(function() {
+    var generateInterval = setInterval(function() {
+        var date = new Date();
         // Set name
-        var generatedName = generateName(tick, 15, 4);
+        var generatedName = generateName(tick, 15, 8);
         $("#name").text(generatedName);
         // Stop interval when name is filled in
-        if (generatedName == name) clearInterval(interval);
+        if (generatedName == name) clearInterval(generateInterval);
         tick++;
     }, 40);
+
+    setTimeout(function() {
+        setBackground();
+        var bgInterval = setInterval(function() {
+            setBackground();
+        }, 8000);
+    }, 4575);
+
 
 });
